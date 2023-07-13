4ee6cf061ee54dddcc3d6a6aa8211273f0013d60
Kaic <136115920+KaicCode@users.noreply.github.com>
2023-07-12 21:12:10 -0300

    Tipos de dados

--- 4-codigo/js/script.js
+++ 4-codigo/js/script.js
@@ -9,3 +9,35 @@ console.log(nome)
 console.log(typeof nome)
 
 document.write("Testando o document.write")
+
+//Boolean
+
+var verdadeiro = true;
+console.log(verdadeiro);
+console.log(typeof verdadeiro)
+
+var falso = false;
+console.log(falso);
+console.log(typeof falso)
+
+// Null e Undefined
+
+var nome1 = null
+console.log(nome1)
+
+nome1 = "KAIC"
+console.log(nome)
+
+// Object
+
+var obj = {
+    nome: "KAIC",
+    idade: 18,
+    profisao: "Programador",
+    estaTrabalhando: false
+};
+
+console.log(obj);
+console.log(typeof obj);
+console.log("Meu nome é: " + obj.nome)
+console.log("Está Trabalhando: " + obj.estaTrabalhando)
